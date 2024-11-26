e4b316413634295557f769ea7178b920b6be7da1
David Murphy <damurphy@vmware.com>
2024-11-26 11:40:09 -0700

    Updated containers, and added back in AlmaLinux

--- .github/workflows/templates/generate.py
+++ .github/workflows/templates/generate.py
@@ -8,15 +8,15 @@ os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 # only test against current containers with systemd
 # will add these when they become available with systemd
+#    "amazonlinux-2",
+#    "amazonlinux-2023",
+#    "debian-11",
 #    "debian-13",
 #    "fedora-40",
 #    "photon-4",
 #    "ubuntu-2004",
 #    "ubuntu-2404",
 LINUX_DISTROS = [
-    "amazonlinux-2",
-    "amazonlinux-2023",
-    "debian-11",
     "debian-12",
     "photon-5",
     "rockylinux-8",
@@ -36,15 +36,15 @@ OSX = [
 
 # only test against current containers with systemd
 # will add these when they become available with systemd
+#    "amazonlinux-2",
+#    "amazonlinux-2023",
+#    "debian-11",
 #    "debian-13",
 #    "fedora-40",
 #    "photon-4",
 #    "ubuntu-2004",
 #    "ubuntu-2404",
 STABLE_DISTROS = [
-    "amazonlinux-2",
-    "amazonlinux-2023",
-    "debian-11",
     "debian-12",
     "photon-5",
     "rockylinux-8",
@@ -54,15 +54,15 @@ STABLE_DISTROS = [
 
 # only test against current containers with systemd
 # will add these when they become available with systemd
+#    "amazonlinux-2",
+#    "amazonlinux-2023",
+#    "debian-11",
 #    "debian-13",
 #    "fedora-40",
 #    "photon-4",
 #    "ubuntu-2004",
 #    "ubuntu-2404",
 ONEDIR_DISTROS = [
-    "amazonlinux-2",
-    "amazonlinux-2023",
-    "debian-11",
     "debian-12",
     "photon-5",
     "rockylinux-8",
